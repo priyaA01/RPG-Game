9e89f494f28822cc3ebb8426cd860a37b4523228
priyaA01 <pryshanthi.it@gmail.com>
2018-01-24 21:19:20 -0600

    completed functionality

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# week-4-game-2
+#Banana Wars 
 RPG game
+ !
+Banana Wars is a role based game built using jQuery,HTML and CSS. 
+
+##Getting Started
+-The game starts allowing the player to choose his character by clicking on the minions pictures.The player will fight as that character for the rest of the game.
+-The player must defeat all of the remaining minions.Enemy minions should be moved to a different area of the screen.
+-Then the player chooses an opponent by clicking on an enemy's picture.
+-Once the player selects an opponent, that enemy is moved to a defender area.
+-The player will now be able to click the attack button.
+	1.Whenever the player clicks attack, their character damages the defender. The opponent will lose bananas (health points). These counts are displayed at the 		bottom of the defender's picture.
+	2.The opponent character will instantly counter the attack. When that happens, the player's character will lose some of their banana. These points are shown at 	the bottom of the player character's picture.
+-The player will keep hitting the attack button in an effort to defeat their opponent.
+	1.When the defender's banana count is reduced to zero or below, remove the enemy from the defender area. The player character can now choose a new opponent.
+-The player wins the game by defeating all enemy characters. The player loses the game if their character's banana count falls to zero or below.
+
+###Prerequisites
+None.
+
+####Author
+Priya Ammaiyappan
+
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,178 +1,199 @@
+/*global variables*/
+var attackPower = 0;
+var attackMsg = "";
 
-var attackPower=0;
-var attackMsg="";
+/*character object containing character name , healthpoints, base attack power and counter attack power.
+object method setPower returns player's attackpowers*/
+var characterObject = {
 
+	"character1": {
+		"name": "Obi-Wan Kenobi",
+		"healthPoints": 120,
+		"attackPower": 8,
+		"counterAttackPower": 15
+	},
+	"character2": {
+		"name": "Luke Skywalker",
+		"healthPoints": 100,
+		"attackPower": 15,
+		"counterAttackPower": 5
+	},
+	"character3": {
+		"name": "Darth Sidious",
+		"healthPoints": 150,
+		"attackPower": 6,
+		"counterAttackPower": 20
+	},
+	"character4": {
+		"name": "Darth Maul",
+		"healthPoints": 180,
+		"attackPower": 3,
+		"counterAttackPower": 25
+	},
 
-var characters = $("#divCharacter").children("a");
+	/*function to get the attack powers for player and defender*/
 
-var characterObject={
+	setPower: function (player) {
+		/*array to store baseattackpower and counterattackpower*/
+		var attackPower = [];
 
-		"character1":{
-			"name":"Obi-Wan Kenobi",
-			"healthPoints":120,
-			"attackPower":8,
-			"counterAttackPower":15
-		},
-		"character2":{
-			"name":"Luke Skywalker",
-			"healthPoints":100,
-			"attackPower":15,
-			"counterAttackPower":5
-		},
-		"character3":{
-			"name":"Darth Sidious",
-			"healthPoints":150,
-			"attackPower":6,
-			"counterAttackPower":20
-		},
-		"character4":{
-			"name":"Darth Maul",
-			"healthPoints":180,
-			"attackPower":3,
-			"counterAttackPower":25
-		},
+		if (player === "c1") {
+			attackPower[2] = this.character1.attackPower;
+			attackPower[3] = this.character1.counterAttackPower;
 
-		/*function to get the attack powers for player and defender*/
+		}
+		if (player === "c2") {
+			attackPower[2] = this.character2.attackPower;
+			attackPower[3] = this.character2.counterAttackPower;
+		}
+		if (player === "c3") {
+			attackPower[2] = this.character3.attackPower;
+			attackPower[3] = this.character3.counterAttackPower;
+		}
+		if (player === "c4") {
+			attackPower[2] = this.character4.attackPower;
+			attackPower[3] = this.character4.counterAttackPower;
+		}
 
-		setPower:function(player)
-		{
-			var attackPower=[];	
-			//var baseAttackPower, counterAttackPower;
+		return attackPower;
 
-			if(player==="c1")
-			{
-				attackPower[0]=this.character1.name;
-				attackPower[1]=this.character1.healthPoints;
-				attackPower[2]=this.character1.attackPower;
-				attackPower[3]=this.character1.counterAttackPower;	
-				
-			}
-			if(player==="c2"){
-				attackPower[0]=this.character2.name;
-				attackPower[1]=this.character2.healthPoints;
-				attackPower[2]=this.character2.attackPower;
-				attackPower[3]=this.character2.counterAttackPower;	
-			}
-			if(player==="c3"){
-				attackPower[0]=this.character3.name;
-				attackPower[1]=this.character3.healthPoints;
-				attackPower[2]=this.character3.attackPower;
-				attackPower[3]=this.character3.counterAttackPower;	
-			}
-			if(player==="c4"){
-				attackPower[0]=this.character4.name;
-				attackPower[1]=this.character4.healthPoints;
-				attackPower[2]=this.character4.attackPower;
-				attackPower[3]=this.character4.counterAttackPower;	
-			}
-
-			return attackPower;
-
-		},		
+	},
 
 };
 
 /*function to choose your character and enemies 
 and also to choose defender from the enemies*/
-//$('#a').on('click', '.b', function(){
-//$(document.body).on("click","#divCharacter",function(){
-$("#divCharacter").children("a").on("click",function(){
-//$("#divCharacter").on("click",".character" , function(){
 
-    $("#divCharacter").empty();
-    $("#enemies").append(characters);
-    $("#divCharacter").prepend($(this));
-    $("#enemies").children("a").children("img").css({"background-color": "red", "border": "3px solid black"});
+$('#divCharacter').on('click', 'a', function () {
 
-   $("#enemies").children("a").on("click",function(){
+	$("#hasEnemies").css({
+		"display": "block"
+	});
+	var character = $("#divCharacter").children("a");
+	$("#divCharacter").empty();
+	$("#enemies").append(character);
+	$("#divCharacter").prepend($(this));
 
-	    if($("#defender").children().length<=0)
-	    {
+	$("#enemies").children("a").children("img").css({
+		"background-color": "red",
+		"border": "3px solid black"
+	});
+
+	$("#enemies").children("a").on("click", function () {
+
+		if ($("#defender").children().length <= 0) {
 			$("#defender").append($(this));
-			$("#defender").children("a").children("img").css({"color":"white","background-color": "black", "border": "3px solid green"});
-			$("#defender").children("a").children("span").css({"color":"white"});
-	    	$("#attack").prop("disabled", false);
-	    	$("#attackMsg").html("");
-	    }	
-    }); 
+			$("#defender").children("a").children("img").css({
+				"color": "white",
+				"background-color": "black",
+				"border": "3px solid green"
+			});
+			$("#defender").children("a").children("span").css({
+				"color": "white"
+			});
+			$("#attack").prop("disabled", false);
+			$("#attackMsg").html("");
+			$("#hasDefender").css({
+				"display": "block"
+			});
+		}
+	});
 
 });
 
-/*function for attack*/
-	
-$("#attack").on("click",function(){
-	var playerHealth=$("#divCharacter").children("a").children(".bottomWords").html();
-	var defenderHealth=$("#defender").children("a").children(".bottomWords").html();
-	var playerName=$("#divCharacter").children("a").children(".topWords").html();
-	var defenderName=$("#defender").children("a").children(".topWords").html();
+/*function for attack changes health points of player and defender based on defenders counterattackpower and player's attackpower respectively.
+once all enemies are moved one by one to dfender area and been defeated , the player wins. if 
+any of the defender defeats player the n the player losses the game. player win / loss the game is restarted
+setting everything to game start*/
 
-	var player=$("#divCharacter").children("a").attr("id");
-	var defender=$("#defender").children("a").attr("id");
+$("#attack").on("click", function () {
+	var playerHealth = $("#divCharacter").children("a").children(".bottomWords").html();
+	var defenderHealth = $("#defender").children("a").children(".bottomWords").html();
+	var playerName = $("#divCharacter").children("a").children(".topWords").html();
+	var defenderName = $("#defender").children("a").children(".topWords").html();
 
-	var baseAttackPower= characterObject.setPower(player);
-	var counterAttackPower= characterObject.setPower(defender);;
+	var player = $("#divCharacter").children("a").attr("id");
+	var defender = $("#defender").children("a").attr("id");
 
-	attackPower+=baseAttackPower[2];
-	
-	if($("#defender").children().length<=0  && $("#enemies").children().length>0)
-	{
-		$("#attackMsg").html("No Defender to attack");
-	}
-	else if($("#defender").children().length > 0 )
-	{
-		attackMsg= "You attacked  " +defenderName + " for  " +attackPower + " damage. <br>"+ defenderName + " attacked you back for " + counterAttackPower[3]+ " damage.";
-		defenderHealth-=attackPower;
-		
-		if (defenderHealth<=0 )
-		{
-			if($("#enemies").children().length>0)
-			{
-			attackMsg="You have defeated " + defenderName + ". You can choose to fight another enemy.";
-			$("#defender").children("a").remove();
-			}
-			else
-			{
-				attackMsg="You won ..... GAME OVER !!";
+	var baseAttackPower = characterObject.setPower(player);
+	var counterAttackPower = characterObject.setPower(defender);;
+
+	attackPower += baseAttackPower[2];
+
+	if ($("#defender").children().length <= 0 && $("#enemies").children().length > 0) {
+		$("#attackMsg").html("Po-ka, Bi-do ");
+	} else if ($("#defender").children().length > 0) {
+		attackMsg = "You attacked  " + defenderName + " for  " + attackPower + " bananas. <br>" + defenderName + " attacked you back for " + counterAttackPower[3] + " bananas.";
+		defenderHealth -= attackPower;
+
+		if (defenderHealth <= 0) {
+			if ($("#enemies").children().length > 0) {
+				attackMsg = "You have eaten all bananas from " + defenderName + ". Choose another buddy.";
+				$("#hasDefender").css({
+					"display": "none"
+				});
+				$("#defender").children("a").remove();
+			} else {
+				$("#hasDefender").css({
+					"display": "none"
+				});
+				$("#hasEnemies").css({
+					"display": "none"
+				});
+				attackMsg = "Tulaliloo ti amo Banana King " + playerName + "..... You Won !!";
 				$("#attack").prop("disabled", true);
 				$("#defender").children("a").remove();
-				 restart();
+				restart();
 			}
-	    	
-		}	
-		else
-		{
-			playerHealth-=counterAttackPower[3];
-			if(playerHealth<=0)
-			{
-				attackMsg="You been defeated ..... GAME OVER !!";
+
+		} else {
+			playerHealth -= counterAttackPower[3];
+			if (playerHealth <= 0) {
+				attackMsg = "You lost all your bananas, Poopaye ! ";
 				$("#attack").prop("disabled", true);
 				restart();
 			}
 		}
 		$("#divCharacter").children("a").children(".bottomWords").html(playerHealth);
-		$("#defender").children("a").children(".bottomWords").html(defenderHealth);		
+		$("#defender").children("a").children(".bottomWords").html(defenderHealth);
 		$("#attackMsg").html(attackMsg);
 	}
-	
+
 
 });
 
 /*function to create restart button to restart the game*/
-function restart()
-{
-	$("#restart").css({"display":"block"});
-	$("#restart").on("click",function(){
+function restart() {
+	$("#restart").css({
+		"display": "block"
+	});
+
+	$("#restart").on("click", function () {
 		$("#divCharacter").empty();
 		$("#enemies").empty();
 		$("#defender").empty();
 		$("#attackMsg").html("");
-		$("#divCharacter").prepend('<span class="words">Your Character</span>');
-		$("#divCharacter").append('<a href="#" class ="character" id="c1"><img src="assets/images/character1.jfif" alt="character1" > <span class="topWords">Obi-Wan Kenobi</span><span class="bottomWords">'+120+'</span></a> <a href="#" class ="character" id="c2"><img src="assets/images/character2.jfif" alt="character2" > <span class="topWords">Luke Skywalker</span><span class="bottomWords">'+100+'</span></a> <a href="#" class ="character" id="c3"><img src="assets/images/character3.jfif" alt="character3" > <span class="topWords">Darth Sidious</span><span class="bottomWords">'+150+'</span></a> <a href="#" class ="character" id="c4"><img src="assets/images/character4.jfif" alt="character4" > <span class="topWords">Darth Maul</span><span class="bottomWords">'+180+'</span></a>'); 
-		$("#divCharacter").children("a").children("img").css({"background-color": "white", "border": "3px solid green"});
-		$("#restart").css({"display":"none"});
+		attackPower = 0;
+		$("#divCharacter").append('<a href="#" class ="character" id="c1"> <img src="assets/images/character1.jpg" alt="character1" > <span class="topWords">Kevin</span> <span class="bottomWords">120</span></a>');
+
+		$("#divCharacter").append('<a href="#" class ="character" id="c2"><img src="assets/images/character2.jpg" alt="character2" > <span class="topWords">Bob</span><span class="bottomWords">100</span></a>');
+
+		$("#divCharacter").append('<a href="#" class ="character" id="c3"><img src="assets/images/character3.jpg" alt="character3" > <span class="topWords">Dave</span><span class="bottomWords">150</span></a>');
+
+		$("#divCharacter").append('<a href="#" class ="character" id="c4"><img src="assets/images/character4.jpg" alt="character4" > <span class="topWords">Stuart</span><span class="bottomWords">180</span></a>');
+
+		$("#divCharacter").children("a").children("img").css({
+			"border": "3px solid red"
+		});
+		$("#hasEnemies").css({
+			"display": "none"
+		});
+		$("#hasDefender").css({
+			"display": "none"
+		});
+		$("#restart").css({
+			"display": "none"
+		});
 	});
 
 }
-/*var characterId=$(this).attr("id");
-	console.log(characterId);	
-	*/
